25040a1d1603a1ef95b12d0e1d6119f7684dc5aa
chypacs <chylene.saraspe.learn@gmail.com>
2023-01-24 06:18:44 +0800

    Sample config

---
+++ config-sample.php
@@ -0,0 +1,22 @@
+<?php
+
+    define("PAGE_TITLE", "BISU-ARMS");
+    define("AACCUP_FILES", './AACCUP_FILES');
+
+    define("ADMIN_USERNAME", '');
+    define("ADMIN_PASSWORD", '');
+    define("DB_NAME", 'bisu_arms');
+    define("DB_HOST", 'localhost');
+    define("DB_USER", "");
+    define("DB_PASS", "");
+
+    # Department List
+    define('ICT', 'Information & Communications Technology Deparment');
+    define('ITD', 'Industrial Technology Deparment');
+
+    # Indicators
+    define('SIP', 'SYSTEM INPUTS AND PROCESSES');
+    define('IMP', 'IMPLEMENTATION');
+    define('OUT', 'OUTCOME/S');
+
+?>
